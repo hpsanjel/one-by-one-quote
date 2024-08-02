48d682228ba79c712224177633c887c9ef81689d
Hari Prasad Sanjel <121128703+hpsanjel@users.noreply.github.com>
2024-08-02 23:35:13 +0200

    updated vite-config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
-	base: "/one-by-one-quote/",
+	base: "/RANDOM-QUOTE-GENERATOR/",
 	build: {
 		outDir: "dist",
 	},

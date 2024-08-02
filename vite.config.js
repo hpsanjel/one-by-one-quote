b1b749d9cf132841aa230d92d446bf0ec27f0f57
Hari Prasad Sanjel <121128703+hpsanjel@users.noreply.github.com>
2024-08-02 22:51:37 +0200

    added configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	base: "/Random-Quote-Generator/", // Adjust this to your repository name
+	build: {
+		outDir: "dist",
+	},
+});
